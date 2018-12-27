36f9218ea050c01f9038e2298828f7db5de845fe
denzel <422293027@qq.com>
2018-12-27 10:57:05 +0800

    dev

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css'
+
+const MyComponent = () => (
+  <div>
+    <span className={"text"}>hello myComponent</span>
+    <span className={"bg"}></span>
+  </div>
+)
+
+export default MyComponent;
